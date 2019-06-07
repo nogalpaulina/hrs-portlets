aacac5b2838c6ee7128ff01f1f6804114b06d188
Andrew Petro <andrew.petro@wisc.edu>
2019-06-07 11:23:13 -0500

    fix: add ResourceMapping import in BenefitInformationController

--- hrs-portlets-webapp/src/main/java/edu/wisc/portlet/hrs/web/benefits/BenefitInformationController.java
+++ hrs-portlets-webapp/src/main/java/edu/wisc/portlet/hrs/web/benefits/BenefitInformationController.java
@@ -28,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
 
 import edu.wisc.hr.dao.bnsumm.BenefitSummaryDao;
 import edu.wisc.hr.dm.bnsumm.BenefitSummary;
