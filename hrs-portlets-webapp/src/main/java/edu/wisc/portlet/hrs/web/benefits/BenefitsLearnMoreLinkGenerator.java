3e1fb9a72be77d3d85a194c2dfc00c025d1f72a8
Andrew Petro <andrew.petro@wisc.edu>
2019-06-18 15:34:22 -0500

    handle the 3 Madison learnMore link cases

--- hrs-portlets-webapp/src/main/java/edu/wisc/portlet/hrs/web/benefits/BenefitsLearnMoreLinkGenerator.java
+++ hrs-portlets-webapp/src/main/java/edu/wisc/portlet/hrs/web/benefits/BenefitsLearnMoreLinkGenerator.java
@@ -5,7 +5,23 @@ import java.util.Set;
 public class BenefitsLearnMoreLinkGenerator {
 
   public String learnMoreLinkFor(Set<String> roles, boolean madisonness) {
-    return "https://www.wisconsin.edu/ohrwd/benefits/";
+
+    if (!madisonness) {
+      // regardless of situation, non-madison employees get this learn more link
+      return "https://www.wisconsin.edu/ohrwd/benefits/";
+    } else if (roles.contains("ROLE_VIEW_NEW_HIRE_BENEFITS")) {
+      // madison users with a new hire event get this learn more link
+      return "https://hr.wisc.edu/benefits/new-employee-benefits-enrollment/";
+    } else if (roles.contains("ROLE_VIEW_OPEN_ENROLL_BENEFITS")) {
+      // madison users with an annual benefit enrollment opportunity get this
+      // learn more link
+      return "https://hr.wisc.edu/benefits/annual-benefits-enrollment/";
+    } else {
+      // madison users without an enrollment opportunity get this learn more
+      // link
+      return "http://benefits.wisc.edu/";
+    }
+
   }
 
 }
--- hrs-portlets-webapp/src/test/java/edu/wisc/portlet/hrs/web/benefits/BenefitsLearnMoreLinkGeneratorTest.java
+++ hrs-portlets-webapp/src/test/java/edu/wisc/portlet/hrs/web/benefits/BenefitsLearnMoreLinkGeneratorTest.java
@@ -1,6 +1,7 @@
 package edu.wisc.portlet.hrs.web.benefits;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -17,6 +18,45 @@ public class BenefitsLearnMoreLinkGeneratorTest {
     assertEquals("https://www.wisconsin.edu/ohrwd/benefits/",
       generator.learnMoreLinkFor(new HashSet<String>(), false));
 
+  }
+
+  @Test
+  public void testRolelessMadisonUser() {
+
+    assertEquals("http://benefits.wisc.edu/",
+      generator.learnMoreLinkFor(new HashSet<String>(), true));
+
+  }
+
+  @Test
+  public void testAnnualBenefitEnrollmentOpportunityMadisonUser() {
+    Set<String> roles = new HashSet<String>();
+    roles.add("ROLE_VIEW_OPEN_ENROLL_BENEFITS");
+
+    assertEquals("https://hr.wisc.edu/benefits/annual-benefits-enrollment/",
+      generator.learnMoreLinkFor(roles, true));
+
+  }
+
+  @Test
+  public void testNewHireBenefitEnrollmentOpportunityMadisonUser() {
+    Set<String> roles = new HashSet<String>();
+    roles.add("ROLE_VIEW_NEW_HIRE_BENEFITS");
+
+    assertEquals(
+      "https://hr.wisc.edu/benefits/new-employee-benefits-enrollment/",
+      generator.learnMoreLinkFor(roles, true));
+  }
+
+  @Test
+  public void testMadisonNewHireLearnMorePreferredOverAnnualLearnMore() {
+    Set<String> roles = new HashSet<String>();
+    roles.add("ROLE_VIEW_NEW_HIRE_BENEFITS");
+    roles.add("ROLE_VIEW_OPEN_ENROLL_BENEFITS");
+
+    assertEquals(
+      "https://hr.wisc.edu/benefits/new-employee-benefits-enrollment/",
+      generator.learnMoreLinkFor(roles, true));
   }
 
 }
