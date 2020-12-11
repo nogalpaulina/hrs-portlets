3f72605fc0b5d1258d6f88e1028a279edef3f5e5
Andrew Petro <andrew.petro@wisc.edu>
2020-12-11 14:29:41 -0600

    style: normalize whitespace

--- hrs-portlets-webapp/src/main/java/edu/wisc/portlet/hrs/web/contactinfo/ContactInfoController.java
+++ hrs-portlets-webapp/src/main/java/edu/wisc/portlet/hrs/web/contactinfo/ContactInfoController.java
@@ -58,12 +58,12 @@ public class ContactInfoController extends HrsControllerBase {
 	private final String PVI_ATTR = "wiscedupvi";
     private final EmailValidator emailValidator = EmailValidator.getInstance();
     private static final String HR_CONTACT_URL_PREFERENCE_NAME="humanResourceContactUrl";
-    
+
     private String businessEmailRolesPreferences = "businessEmailRoles";
     private ContactInfoDao contactInfoDao;
     private BusinessEmailUpdateDao businessEmailUpdateDao;
     private PreferredNameService preferredNameService;
-    
+
     public void setBusinessEmailRolesPreferences(String businessEmailRolesPreferences) {
         this.businessEmailRolesPreferences = businessEmailRolesPreferences;
     }
@@ -72,7 +72,7 @@ public class ContactInfoController extends HrsControllerBase {
     public void setBusinessEmailUpdateDao(BusinessEmailUpdateDao businessEmailUpdateDao) {
         this.businessEmailUpdateDao = businessEmailUpdateDao;
     }
-    
+
     @Autowired
     public void setPreferredNameService(PreferredNameService service) {
     	this.preferredNameService = service;
@@ -82,7 +82,7 @@ public class ContactInfoController extends HrsControllerBase {
     public void setContactInfoDao(ContactInfoDao contactInfoDao) {
         this.contactInfoDao = contactInfoDao;
     }
-    
+
     /**
      * Populate the ModelMap with the link of humanResourceOffice
      * @param request
@@ -97,59 +97,59 @@ public class ContactInfoController extends HrsControllerBase {
     @RenderMapping
     public String viewContactInfo(ModelMap model, PortletRequest request) {
         final String emplId = PrimaryAttributeUtils.getPrimaryId();
-        
+
         final PersonInformation contactInformation = this.contactInfoDao.getPersonalData(emplId);
         model.addAttribute("contactInformation", contactInformation);
-        
+
         final boolean showBusinessEmail = showBusinessEmail(request);
         model.addAttribute("showBusinessEmail", showBusinessEmail);
         if (showBusinessEmail) {
             final PreferredEmail preferredEmail = this.businessEmailUpdateDao.getPreferedEmail(emplId);
             model.addAttribute("preferredEmail", preferredEmail);
         }
-        
+
         setupPreferredName(model, request);
-        
-        
-        
+
+
+
         return "contactInfo";
     }
-    
+
     private void setupPreferredName(ModelMap modelMap, PortletRequest request) {
     	@SuppressWarnings("unchecked")
 		Map<String, String> userInfo = (Map <String, String>) request.getAttribute(PortletRequest.USER_INFO);
-    	
+
     	modelMap.addAttribute("displayName",userInfo.get("displayName"));//for system
         modelMap.addAttribute("legalName",userInfo.get("wiscEduSORName")); // for uw-madison
         boolean isPreferredNameEnabled = !StringUtils.isBlank(userInfo.get("wiscEduSORName"));
         modelMap.addAttribute("isPreferredNameEnabled", isPreferredNameEnabled);
-        
+
         if(isPreferredNameEnabled) {
     		PreferredName preferredName = preferredNameService.getPreferredName(getPvi(request));
-    		
+
     		String currentFirstName = userInfo.get("wiscedupreferredfirstname");
     		String currentMiddleName = userInfo.get("wiscedupreferredmiddlename");
     		String currentLastName = userInfo.get("wiscedupreferredlastname");
-    		
+
     		if(preferredName != null) {
     			modelMap.addAttribute("firstName", preferredName.getFirstName());
     			modelMap.addAttribute("middleName", preferredName.getMiddleName());
     			modelMap.addAttribute("lastName", preferredName.getLastName());
     		}
-    		
+
     		modelMap.addAttribute("pendingStatus",preferredNameService.getStatus(new PreferredName(currentFirstName, currentMiddleName,currentLastName,getPvi(request)),preferredNameService.getPreferredName(getPvi(request))));
     		modelMap.addAttribute("sirName",userInfo.get("sn"));
-    		
+
     		//edit setup
     		if(!modelMap.containsKey("preferredName")) {
-    		
+
     			if(preferredName != null) {
     				modelMap.addAttribute("preferredName", preferredName);
     			} else {
     				modelMap.addAttribute("preferredName", new PreferredName());
     			}
     		}
-    		
+
     		if(request.getParameter("therewasanerror") != null) {
     			modelMap.addAttribute("therewasanerror","true");
     		}
@@ -160,35 +160,35 @@ public class ContactInfoController extends HrsControllerBase {
     protected boolean showBusinessEmail(PortletRequest request) {
         final PortletPreferences preferences = request.getPreferences();
         final String[] roles = preferences.getValues(this.businessEmailRolesPreferences, new String[0]);
-        
+
         for (final String businessEmailRole : roles) {
             if (request.isUserInRole(businessEmailRole)) {
                 this.logger.debug("Showing business email info, user is in role: {}", businessEmailRole);
                 return true;
             }
         }
-        
+
         this.logger.debug("Hiding business email info, user is not in any of the roles: {}", Arrays.asList(roles));
-        
+
         return false;
     }
-    
+
     @ResourceMapping("businessEmailAddress")
     public String updateBusinessEmailAddress(
             @RequestParam("email") String email,
             ResourceRequest request,
             ModelMap modelMap) throws PortletRequestMethodNotSupportedException {
-        
+
         //TODO can I move this into the annotations?
         //TODO add method support to portlet annotations in portlet contrib
         if (!"POST".equals(request.getMethod())) {
             throw new PortletRequestMethodNotSupportedException(request.getMethod(), new String[] { "POST" });
         }
-        
+
         final String emplid = PrimaryAttributeUtils.getPrimaryId();
-        
+
         email = email.trim();
-        
+
         if (!StringUtils.isEmpty(email)) {
             if (emailValidator.isValid(email)) {
                 this.businessEmailUpdateDao.updatePreferedEmail(emplid, email);
@@ -197,25 +197,25 @@ public class ContactInfoController extends HrsControllerBase {
                 logger.warn("User {} submitted invalid email address {} the change request will be ignored.", emplid, email);
             }
         }
-        
+
         return this.getBusinessEmailAddress(modelMap);
     }
-    
+
     protected String getBusinessEmailAddress( ModelMap modelMap) {
-        
+
         final String emplid = PrimaryAttributeUtils.getPrimaryId();
-        
+
         final PreferredEmail preferredEmail = this.businessEmailUpdateDao.getPreferedEmail(emplid);
         final String updatedEmail = preferredEmail.getEmail();
         modelMap.addAttribute("email", updatedEmail);
-        
+
         return "jsonView";
     }
-    
+
     private String getPvi(PortletRequest request) {
     	@SuppressWarnings("unchecked")
 		Map<String, String> userInfo = (Map <String, String>) request.getAttribute(PortletRequest.USER_INFO);
-    	
+
     	final String pvi = userInfo.get(PVI_ATTR);
     	return pvi;
     }
