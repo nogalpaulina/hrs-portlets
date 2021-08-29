1619e14fc32daf2556e655bcbc0282caa6ba5914
Andrew Petro <andrew.petro@wisc.edu>
2021-08-28 21:38:43 -0500

    Update tests to reflect 2021 Annual Benefits Enrollment dates.

--- hrs-portlets-api/src/test/java/edu/wisc/hr/service/benefits/AnnualBenefitEnrollmentDatesServiceTest.java
+++ hrs-portlets-api/src/test/java/edu/wisc/hr/service/benefits/AnnualBenefitEnrollmentDatesServiceTest.java
@@ -12,69 +12,69 @@ public class AnnualBenefitEnrollmentDatesServiceTest {
   @Test
   public void testForeshadowing() {
     // foreshadowing does not begin prematurely
-    assertFalse(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2020-09-15")));
+    assertFalse(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2021-09-18")));
 
-    // foreshadowing begins Wednesday Sept 16
-    assertTrue(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2020-09-16")));
+    // foreshadowing begins Sunday Sept 19 2021
+    assertTrue(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2021-09-19")));
 
     // and continues
     assertTrue(service.foreshadowAnnualBenefitsEnrollment(AnnualBenefitEnrollmentDatesService.DATE_DURING_ABE_FORESHADOWING));
 
     // through the day before benefit enrollment begins
-    assertTrue(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2020-09-27")));
+    assertTrue(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2021-09-26")));
 
     // and stops once benefit enrollment begins
-    assertFalse(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2020-09-28")));
+    assertFalse(service.foreshadowAnnualBenefitsEnrollment(new LocalDate("2021-09-27")));
   }
 
   @Test
   public void testDuringAnnualBenefitsEnrollment() {
     // annual benefits enrollment does not begin prematurely
-    assertFalse(service.duringAnnualBenefitsEnrollment(new LocalDate("2020-09-27")));
+    assertFalse(service.duringAnnualBenefitsEnrollment(new LocalDate("2021-09-26")));
 
     // but does begin punctually
-    assertTrue(service.duringAnnualBenefitsEnrollment(new LocalDate("2020-09-28")));
+    assertTrue(service.duringAnnualBenefitsEnrollment(new LocalDate("2021-09-27")));
 
     // and continues
-    assertTrue(service.duringAnnualBenefitsEnrollment(new LocalDate("2020-10-05")));
+    assertTrue(service.duringAnnualBenefitsEnrollment(new LocalDate("2021-10-05")));
 
     assertTrue(
       "ABE dates service should consider last date of ABE to be during ABE.",
-      service.duringAnnualBenefitsEnrollment(new LocalDate("2020-10-23")));
+      service.duringAnnualBenefitsEnrollment(new LocalDate("2021-10-22")));
 
     assertFalse(
       "ABE dates service should consider the day after ABE ends to NOT be during ABE",
-      service.duringAnnualBenefitsEnrollment(new LocalDate("2020-10-24")));
+      service.duringAnnualBenefitsEnrollment(new LocalDate("2021-10-23")));
   }
 
   @Test
   public void testLastDayAnnualBenefitsEnrollment() {
     // the day before the last day is not the last day
-    assertFalse(service.lastDayOfAnnualBenefitEnrollment(new LocalDate("2020-10-22")));
+    assertFalse(service.lastDayOfAnnualBenefitEnrollment(new LocalDate("2021-10-21")));
 
     // the last day is the last day
-    assertTrue(service.lastDayOfAnnualBenefitEnrollment(new LocalDate("2020-10-23")));
+    assertTrue(service.lastDayOfAnnualBenefitEnrollment(new LocalDate("2021-10-22")));
 
     // the day after the last day is not the last day
-    assertFalse(service.lastDayOfAnnualBenefitEnrollment(new LocalDate("2020-10-24")));
+    assertFalse(service.lastDayOfAnnualBenefitEnrollment(new LocalDate("2021-10-23")));
   }
 
   @Test
   public void testFeedbackPeriod() {
     // the last day of benefit enrollment is not the feedback period
-    assertFalse(service.feedbackPeriod(new LocalDate("2020-10-23")));
+    assertFalse(service.feedbackPeriod(new LocalDate("2021-10-22")));
 
     // but the day after benefit enrollment is within the feedback period
-    assertTrue(service.feedbackPeriod(new LocalDate("2020-10-24")));
+    assertTrue(service.feedbackPeriod(new LocalDate("2021-10-23")));
 
     // and the feedback period continues
-    assertTrue(service.feedbackPeriod(new LocalDate("2020-11-12")));
+    assertTrue(service.feedbackPeriod(new LocalDate("2021-11-12")));
 
     // through its last day
-    assertTrue(service.feedbackPeriod(new LocalDate("2020-11-27")));
+    assertTrue(service.feedbackPeriod(new LocalDate("2021-11-27")));
 
     // but not after its last day
-    assertFalse(service.feedbackPeriod(new LocalDate("2019-11-28")));
+    assertFalse(service.feedbackPeriod(new LocalDate("2021-11-28")));
   }
 
   @Test
@@ -82,30 +82,30 @@ public class AnnualBenefitEnrollmentDatesServiceTest {
     assertEquals(
       "before ABE, daysRemaining...() returns -1",
       -1, service.daysRemainingInAnnualBenefitsEnrollment(
-        new LocalDate("2020-09-20")));
+        new LocalDate("2021-09-20")));
 
     assertEquals(
       "On the antepenultimate day of ABE, there are 2 more days remaining.",
       2,
       service.daysRemainingInAnnualBenefitsEnrollment(
-        new LocalDate("2020-10-21")));
+        new LocalDate("2021-10-20")));
 
     assertEquals(
       "On the penultimate day of ABE, there is 1 more day remaining.",
       1,
       service.daysRemainingInAnnualBenefitsEnrollment(
-        new LocalDate("2020-10-22")));
+        new LocalDate("2021-10-21")));
 
     assertEquals(
       "On the last day of ABE, there are 0 more days remaining.",
       0,
       service.daysRemainingInAnnualBenefitsEnrollment(
-        new LocalDate("2020-10-23")));
+        new LocalDate("2021-10-22")));
 
     assertEquals(
       "After ABE, daysRemaining...() returns -1.",
       -1,
-      service.daysRemainingInAnnualBenefitsEnrollment(new LocalDate("2020-10-24")));
+      service.daysRemainingInAnnualBenefitsEnrollment(new LocalDate("2021-10-24")));
   }
 
 }
